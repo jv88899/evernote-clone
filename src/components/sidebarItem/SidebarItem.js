2f30f291a73a74e0388872e1ff4aa8800edd131d
jv88899 <jv88899@gmail.com>
2019-10-11 14:58:30 -0500

    added more functionality to sidebaritem component

--- src/components/sidebarItem/SidebarItem.js
+++ src/components/sidebarItem/SidebarItem.js
@@ -1,15 +1,48 @@
 import React, { Component } from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import styles from './styles'
+import DeleteIcon from '@material-ui/icons/Delete'
 import ListItem from '@material-ui/core/ListItem'
-import ListItemText from '@material-ui/icons/Delete'
+import ListItemText from '@material-ui/core/ListItemText'
 import { removeHTMLTags } from '../../helpers'
 
 class SidebarItem extends Component {
+    deleteNote = note => {
+        if (window.confirm(`Are you sure you want to delete: ${note.title}`)) {
+            this.props.deleteNote(note)
+        }
+    }
+
+    selectNote = (n, i) => {
+        this.props.selectNote(n, i)
+    }
+
     render() {
+        const { index, note, classes, selectedNoteIndex } = this.props
+
         return (
-            <div>
-                Hello from SidebarItem
+            <div key={index}>
+                <ListItem
+                    className={classes.listItem}
+                    selected={selectedNoteIndex === index}
+                    alignItems='flex-start'
+                >
+                    <div
+                        className={classes.textSection}
+                        onClick={ () => this.selectNote(note, index) }
+                    >
+                        <ListItemText
+                            primary={note.title}
+                            secondary={removeHTMLTags(note.body.substring(0, 30)) + '...'}
+                        >
+                        </ListItemText>
+                    </div>
+                    <DeleteIcon
+                        className={classes.deleteIcon}
+                        onClick={ () => this.deleteNote(note) }
+                    >
+                    </DeleteIcon>
+                </ListItem>
             </div>
         )
     }
